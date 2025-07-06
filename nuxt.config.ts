7d38bf2b459ff409e9d2216dbbb4ae7e45d7a9ec
Yoann Gendrey <ygendrey@gmail.com>
2025-07-06 14:14:42 +0200

    fix: review

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: '2025-07-15',
   ssr: false,
   css: ['~/assets/css/main.css'],
   vite:{
